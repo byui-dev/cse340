95ef4bc41aaa7b3e0a16d41f3e1935e6b2ea293c
byui-dev <okdiphala@hotmail.com>
2025-10-13 18:00:51 +0200

    add views/account/dashboard.ejs and update accountController.buildDashboard

--- controllers/accountController.js
+++ controllers/accountController.js
@@ -2,6 +2,13 @@ const { body, validationResult } = require("express-validator")
 const utilities = require(".")
 const accountModel = require("../models/account-model")
 
+// import the jsonwebtoken history
+const jwt = require("jsonwebtoken")
+
+// Load environment variables from .env file
+require("dotenv").config()
+
+
 const validate = {}
 
 /*  **********************************
@@ -110,6 +117,58 @@ validate.checkLoginData = async (req, res, next) => {
     return
   }
   next()
+}
+
+/* ****************************************
+ *  Process login request
+ * ************************************ */
+async function accountLogin(req, res) {
+  let nav = await utilities.getNav()
+  const { account_email, account_password } = req.body
+  const accountData = await accountModel.getAccountByEmail(account_email)
+  if (!accountData) {
+    req.flash("notice", "Please check your credentials and try again.")
+    res.status(400).render("account/login", {
+      title: "Login",
+      nav,
+      errors: null,
+      account_email,
+    })
+    return
+  }
+  try {
+    if (await bcrypt.compare(account_password, accountData.account_password)) {
+      delete accountData.account_password
+      const accessToken = jwt.sign(accountData, process.env.ACCESS_TOKEN_SECRET, { expiresIn: 3600 * 1000 })
+      if(process.env.NODE_ENV === 'development') {
+        res.cookie("jwt", accessToken, { httpOnly: true, maxAge: 3600 * 1000 })
+      } else {
+        res.cookie("jwt", accessToken, { httpOnly: true, secure: true, maxAge: 3600 * 1000 })
+      }
+      return res.redirect("/account/")
+    }
+    else {
+      req.flash("message notice", "Please check your credentials and try again.")
+      res.status(400).render("account/login", {
+        title: "Login",
+        nav,
+        errors: null,
+        account_email,
+      })
+    }
+  } catch (error) {
+    throw new Error('Access Forbidden')
+  }
+}
+
+async function buildDashboard(req, res) {
+  const nav = await utilities.getNav()
+  res.render("account/dashboard", {
+    title: "Dashboard",
+    nav,
+    messages: req.flash(),
+    errors: null
+  })
 }
 
 module.exports = validate
